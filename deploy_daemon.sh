f7cc2e7036b8a99209846c26778a27721f8903b1
Jasper C <37154053+JasperMC@users.noreply.github.com>
2021-07-08 08:43:01 +0200

    Update deploy_daemon.sh

--- deploy_daemon.sh
+++ deploy_daemon.sh
@@ -1,1 +1,30 @@
+#!/bin/sh
+
+sudo apt install git curl
+
+sudo curl -o get-docker.sh https://get.docker.com/
+sh get-docker.sh
+rm get-docker.sh
+sudo mkdir /srv/app
+
+# Check permissions
+sudo chown -R admin:admin /srv/app
+
+cd /srv/app
+
+# Get pterodactyl
+git clone https://github.com/ccarney16/pterodactyl-containers
+
+cd pterodactyl
+
+# Configure pterodactyl
+bin/deploy --daemon
+
+# Edit panel.env
+
+
+# Launch
+sudo docker-compose up -d
+
+
 
